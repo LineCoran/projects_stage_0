7adfaa5b7cc653ca17d79ef832449d052fa70032
LineCoran <vppisya@gmail.com>
2022-07-30 13:58:35 +0300

    fix: audio and quotes links

--- momentum/assets/js/quotes.js
+++ momentum/assets/js/quotes.js
@@ -5,7 +5,7 @@ const quoteAuthor = document.querySelector('.author');
 const quoteButton = document.querySelector('.change-quote');
 
 async function getQuotes() {
-    const quotes = '../../data.json';
+    const quotes = './../../momentum/data.json';
     const res = await fetch(quotes);
     const data = await res.json();
     quoteText.innerHTML = data[getRandomNum(data.length)].text;
